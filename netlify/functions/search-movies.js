6ad5b900eb3c83daae625724d7bb709b62d48c9c
HeadlessRyn <headlessryn@gmail.com>
2025-11-01 02:48:04 -0400

    sigh

--- netlify/functions/search-movies.js
+++ netlify/functions/search-movies.js
@@ -1,9 +1,22 @@
 exports.handler = async (event, context) => {
     const apiKey = process.env.TMDB_KEY;
-    const { query } = event.queryStringParameters;
+    const { query, trending } = event.queryStringParameters;
+
+    let url = '';
+
+    if (trending === 'true') {
+        url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`;
+    } else if (query) {
+        url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Missing query or trending parameter' })
+        };
+    }
 
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`);
+        const response = await fetch(url);
         const data = await response.json();
 
         return {
